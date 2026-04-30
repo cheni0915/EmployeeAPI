3bad4898e326de98df803fab6e54b72d8be6893f
cheni0915 <easy09150915@gmail.com>
2026-04-30 23:59:09 +0800

    first commit

---
+++ Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string Name { get; set; }
+
+
+        [StringLength(50)]
+        public string Department { get; set; } = "";
+
+
+        [StringLength(20)]
+        public string Telephone { get; set; } = "";
+
+
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; } = "";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// 將服務添加到容器中
+builder.Services.AddControllers();
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+// 配置 HTTP 請求管道
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
